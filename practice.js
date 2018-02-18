f2366a7f9170ccd82ec981de9191ea5f5bbb2c1a
chaninkou <michaelkoulove@yahoo.com>
2018-02-17 23:31:00 -0800

    Can't wait to learn more about javascript

---
+++ practice.js
@@ -0,0 +1,14 @@
+// , will count as number
+document.write("1+2 = ", 1 + 2, "<br />");
+
+// + will be a string
+document.write("1+2 = " + 1 + 2, "<br />");
+
+// how to round
+var val1 = 12.1341;
+document.write("How to round: ", val1.toFixed(2), "<br/>");
+
+// shortcut
+var val2 = 2;
+document.write("val2++ will be ", val2++);
+document.write("val2 = ", val2);
