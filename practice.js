0c285203b74fbb0dc8bd8062d3bd1a811a0ce75b
chaninkou <michaelkoulove@yahoo.com>
2018-02-18 19:36:02 -0800

    Need to study for midterms, will work on javascript later

--- practice.js
+++ practice.js
@@ -4,11 +4,23 @@ document.write("1+2 = ", 1 + 2, "<br />");
 // + will be a string
 document.write("1+2 = " + 1 + 2, "<br />");
 
+// the max and min
+document.write("Max number = ", Number.MAX_VALUE, "<br/>");
+document.write("Min number = ", Number.MIN_VALUE, "<br/>");
+
 // how to round
 var val1 = 12.1341;
 document.write("How to round: ", val1.toFixed(2), "<br/>");
 
-// shortcut
+// shortcut(not that useful)
 var val2 = 2;
-document.write("val2++ will be ", val2++);
-document.write("val2 = ", val2);
+document.write("val2++ = ", val2++, "<br />");
+document.write("val2 = ", val2, "<br />");
+
+// more shortcut for numbers(same for -=,*=, /=)
+var val3 = 3;
+document.write("val3 += 3", val3 +=3, "<br />");
+
+// Some math function
+document.write("Math.E = ", Math.E, "<br />");
+document.write("Math.PI ", Math.PI, "<br />");
