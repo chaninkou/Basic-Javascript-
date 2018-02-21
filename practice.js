96f4ac7b16d4fa08aad25e94cce4363fda07554e
chaninkou <michaelkoulove@yahoo.com>
2018-02-20 23:11:22 -0800

    Will work on it later because of midterms

--- practice.js
+++ practice.js
@@ -25,9 +25,11 @@ document.write("val3 += 3", val3 +=3, "<br />");
 document.write("Math.E = ", Math.E, "<br />");
 document.write("Math.PI = ", Math.PI, "<br />");
 document.write("Math.abs(-2) = ", Math.abs(-2), "<br />");
-// cube root
+// cube root, round 
 document.write("Math.cbrt(4) = ", Math.cbrt(4), "<br />");
-document.write("Math.rond(1.23) = ", Math.floor(1.23), "<br />");
-document.write("Math.ceil(1.23) = ", Math.ceil(1.23), "<br />");
 document.write("Math.floor(1.23) = ", Math.floor(1.23), "<br />");
+document.write("Math.ceil(1.23) = ", Math.ceil(1.23), "<br />");
+document.write("Math.round(1.23) = ", Math.round(1.23), "<br />");
+document.write("Math.log(1.23) = ", Math.log(1.23), "<br />");
+document.write("Math.pow(2,3) = ", Math.pow(2,3), "<br />");
 
